e962f687240233be067e9911135db0fcdbfaf593
Rafal Jeczalik <rafal@rjk.io>
2025-07-16 02:57:47 +0200

    mp3 chunking

--- main.go
+++ main.go
@@ -14,44 +14,43 @@ type DataChunk struct {
 }
 
 func main() {
-	bufferSize := flag.Int("b", 8096, "buffer size")
+	var blockSize int
+	flag.IntVar(&blockSize, "b", 8192, "block size for chunking")
 	flag.Parse()
 
 	if flag.NArg() < 1 {
-		fmt.Fprintf(os.Stderr, "Usage: %s [-b buffer_size] <file>\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Usage: %s [-b blocksize] <file>\n", os.Args[0])
 		os.Exit(1)
 	}
 
-	filePath := flag.Arg(0)
-	file, err := os.Open(filePath)
+	filename := flag.Arg(0)
+
+	file, err := os.Open(filename)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error opening file: %v\n", err)
 		os.Exit(1)
 	}
 	defer file.Close()
 
-	buffer := make([]byte, *bufferSize)
-	encoder := json.NewEncoder(os.Stdout)
+	// Create chunker with specified block size and larger reservoir size
+	chunker := NewChunker(file, blockSize, 2048) // Use larger reservoir
 
 	for {
-		bytesRead, err := file.Read(buffer)
+		chunk, err := chunker.Next()
+		if err == io.EOF {
+			break
+		}
 		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
+			fmt.Fprintf(os.Stderr, "Error chunking file: %v\n", err)
 			os.Exit(1)
 		}
 
-		if bytesRead > 0 {
-			chunk := buffer[:bytesRead]
-			encodedData := base64.StdEncoding.EncodeToString(chunk)
-			dataChunk := DataChunk{Data: encodedData}
-
-			if err := encoder.Encode(dataChunk); err != nil {
-				fmt.Fprintf(os.Stderr, "Error encoding JSON: %v\n", err)
-				os.Exit(1)
-			}
+		dataChunk := DataChunk{
+			Data: base64.StdEncoding.EncodeToString(chunk),
+		}
+		if err := json.NewEncoder(os.Stdout).Encode(dataChunk); err != nil {
+			fmt.Fprintf(os.Stderr, "Error encoding JSON: %v\n", err)
+			os.Exit(1)
 		}
 	}
 }

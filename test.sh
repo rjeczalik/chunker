419e55a8f978cc415ee66dabf5545e8aba3e0caf
Rafal Jeczalik <rafal@rjk.io>
2025-07-16 15:54:06 +0200

    add sample

--- test.sh
+++ test.sh
@@ -9,4 +9,7 @@ cargo build
 
 export RUST_LOG=debug
 
-./chunker -b=8192 -type ${sample_type} sample.${sample_type} | ./target/debug/jsonl_player -playback ${sample_type}
+# ./chunker -b=8192 -type mp3 sample.mp3 | ./target/debug/jsonl_player --playback mp3
+# ./chunker -b=8192 -type wav sample.wav | ./target/debug/jsonl_player --playback wav
+
+./chunker -b=8192 -type ${sample_type} sample.${sample_type} | ./target/debug/jsonl_player --playback ${sample_type}
